526940ac9f49d5149c0bdbb155358920a0292f5f
rahul <rahulpattusamy@gmail.com>
2026-02-20 08:20:20 +0530

    Add NavBar component and update layout to include it; add react-icons dependency

---
+++ app/NavBar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { FaBug } from "react-icons/fa";
+
+const NavBar = () => {
+  const Links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {Links.map((l) => (
+          <li key={l.href}>
+            <Link className="" href={l.href}>
+              {l.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
